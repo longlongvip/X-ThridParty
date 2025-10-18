ac95ae42c33e4936eb47d018c704dd9e476ce77a
DESKTOP-U06KDH6\admin <longlongvip@outlook.com>
2025-10-18 21:45:28 +0800

    添加 glad2 lua gsl

--- third.bat
+++ third.bat
@@ -1,20 +1,35 @@
 if exist fmt (
     cd fmt
     git pull
+    cd..
 ) else (
     git clone https://github.com/fmtlib/fmt.git
+    cd..
 )
 
 if exist glfw (
     cd glfw
     git pull
+    cd..
 ) else (
     git clone https://github.com/glfw/glfw.git
+    cd..
 )
 
 if exist imgui (
     cd imgui
     git pull
+    cd..
 ) else (
     git clone https://github.com/ocornut/imgui.git
+    cd..
+)
+
+if exist lua (
+    cd lua
+    git pull
+    cd..
+) else (
+    git clone https://github.com/lua/lua.git
+    cd..
 )
