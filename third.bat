83a247845e5e4647cc32a95576b0d0632b8e578a
DESKTOP-U06KDH6\admin <longlongvip@outlook.com>
2026-03-02 22:10:56 +0800

    优化脚本，增加新的第三方库

--- third.bat
+++ third.bat
@@ -1,35 +1,22 @@
-if exist fmt (
-    cd fmt
-    git pull
-    cd..
-) else (
-    git clone https://github.com/fmtlib/fmt.git
-    cd..
+@echo off
+setlocal enabledelayedexpansion
+
+if not exist repos.txt (
+    echo 错误：repos.txt 文件不存在！
+    exit /b 1
 )
 
-if exist glfw (
-    cd glfw
-    git pull
-    cd..
-) else (
-    git clone https://github.com/glfw/glfw.git
-    cd..
+for /f "usebackq tokens=1,*" %%a in ("repos.txt") do (
+    set dir=%%a
+    set url=%%b
+    if exist !dir! (
+        echo 正在更新 !dir! ...
+        pushd !dir!
+        git pull
+        popd
+    ) else (
+        echo 正在克隆 !dir! 从 !url! ...
+        git clone !url! !dir!
+    )
 )
-
-if exist imgui (
-    cd imgui
-    git pull
-    cd..
-) else (
-    git clone https://github.com/ocornut/imgui.git
-    cd..
-)
-
-if exist lua (
-    cd lua
-    git pull
-    cd..
-) else (
-    git clone https://github.com/lua/lua.git
-    cd..
-)
+echo 所有库处理完成。
